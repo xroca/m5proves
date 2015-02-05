6f6cb2f2d4fb22a84feae3ded563c6df210187f2
DAM <DAM@1smx-16>
2015-02-05 16:17:01 +0100

    fadsfas

---
+++ ProvesGit/src/prova/Principal.java
@@ -0,0 +1,17 @@
+package prova;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Ep hola!");
+		
+		System.out.println("Proves GIT");
+		
+		System.out.println("Proves Git 2");
+		
+	}
+
+}
